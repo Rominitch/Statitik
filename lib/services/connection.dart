5cb88f4b40c531d2031dea0e05a2ee247e7eac3b
Rominitch <rominitch@gmail.com>
2021-06-03 15:22:50 +0200

    [FIX] Fix security

--- lib/services/connection.dart
+++ lib/services/connection.dart
@@ -1,22 +1,12 @@
-import 'package:flutter/foundation.dart';
 import 'package:mysql1/mysql1.dart';
 
-const local = !kReleaseMode;
-const ventoux = true;
-const String adresse     = local ? (ventoux ? '192.168.1.65' : '192.168.73.166'): 'mouca.fr';
-const String adresseHTML = 'https://mouca.fr';
-const int port           = local ? (ventoux ? 3306 : 3307) : 26321;
-
-bool useDebug = local;
-
-// WARNING: NEVER COMMIT
 ConnectionSettings createConnection()
 {
   return new ConnectionSettings(
-      host: adresse,
-      port: port,
-      user: 'StatitikCreator',
-      password: ',}C33]C14Sn0LT:u:S!x',
-      db: useDebug ? (ventoux ? 'statitikpokemon' :'StatitikPokemonDebug') : 'StatitikPokemon'
-  );
+        host: 'localhost',
+        port: 3306,
+        user: 'MyUser',
+        password: 'MyPassword',
+        db: 'MyDatabase'
+    );
 }
